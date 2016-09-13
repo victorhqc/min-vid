87752bae7dae52c286faf9a0ee9161ba703ff565
Dave Justice <davejustishh@gmail.com>
2016-09-13 18:55:36 -0400

    Do not show replay view when minimized
    - fixes #243

--- components/player-view.js
+++ components/player-view.js
@@ -219,7 +219,7 @@ module.exports = React.createClass({
             <GeneralControls {...this.props} isYt={this.isYt} />
           </div>
 
-          <div className={cn('exited', {hidden: !this.hasExited()})}>
+          <div className={cn('exited', {hidden: !this.hasExited() || this.props.minimized})}>
             <div className='row'>
               <button className='exit-replay' onClick={this.replay}></button>
               <button className='exit-close' onClick={this.close}></button>
