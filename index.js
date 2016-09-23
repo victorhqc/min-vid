fd098431a4654acb83283fa3bc1811c23559df31
Dave Justice <davejustishh@gmail.com>
2016-09-22 22:22:40 -0400

    add unLoad handler for uninstalling (#293)
    - should help with issues with watchpost refs #233

--- index.js
+++ index.js
@@ -10,49 +10,58 @@ const getYouTubeUrl = require('./lib/get-youtube-url');
 const getVimeoUrl = require('./lib/get-vimeo-url');
 const launchVideo = require('./lib/launch-video');
 const sendMetricsData = require('./lib/send-metrics-data');
-const initContextMenuHandlers = require('./lib/context-menu-handlers');
+const contextMenuHandlers = require('./lib/context-menu-handlers');
 const panelUtils = require('./lib/panel-utils');
 
-// handle browser resizing
-pageMod.PageMod({
-  include: '*',
-  contentScriptFile: './resize-listener.js?cachebust=' + Date.now(),
-  onAttach: function(worker) {
-    worker.port.on('resized', function() {
-      const panel = panelUtils.getPanel();
-      if (panel && panel.isShowing) panelUtils.redraw();
-    });
-  }
-});
+let browserResizeMod, launchIconsMod;
 
-// add launch icon to video embeds
-pageMod.PageMod({
-  include: '*',
-  contentStyleFile: './icon-overlay.css?cachebust=' + Date.now(),
-  contentScriptFile: './icon-overlay.js?cachebust=' + Date.now(),
-  onAttach: function(worker) {
-    worker.port.on('launch', function(opts) {
-      if (opts.domain.indexOf('youtube.com') > -1) {
-        opts.getUrlFn = getYouTubeUrl;
-        sendMetricsData({
-          object: 'overlay_icon',
-          method: 'launch',
-          domain: opts.domain
-        });
-        launchVideo(opts);
-      } else if (opts.domain.indexOf('vimeo.com')  > -1) {
-        opts.getUrlFn = getVimeoUrl;
-        sendMetricsData({
-          object: 'overlay_icon',
-          method: 'launch',
-          domain: opts.domain
-        });
-        launchVideo(opts);
-      }
-    });
-    worker.port.on('metrics', sendMetricsData);
-  }
-});
+exports.main = function() {
+  // handle browser resizing
+  browserResizeMod = pageMod.PageMod({
+    include: '*',
+    contentScriptFile: './resize-listener.js?cachebust=' + Date.now(),
+    onAttach: function(worker) {
+      worker.port.on('resized', function() {
+        const panel = panelUtils.getPanel();
+        if (panel && panel.isShowing) panelUtils.redraw();
+      });
+    }
+  });
 
-// add 'send-to-mini-player' option to context menu
-initContextMenuHandlers();
+  // add launch icon to video embeds
+  launchIconsMod = pageMod.PageMod({
+    include: '*',
+    contentStyleFile: './icon-overlay.css?cachebust=' + Date.now(),
+    contentScriptFile: './icon-overlay.js?cachebust=' + Date.now(),
+    onAttach: function(worker) {
+      worker.port.on('launch', function(opts) {
+        if (opts.domain.indexOf('youtube.com') > -1) {
+          opts.getUrlFn = getYouTubeUrl;
+          sendMetricsData({
+            object: 'overlay_icon',
+            method: 'launch',
+            domain: opts.domain
+          });
+          launchVideo(opts);
+        } else if (opts.domain.indexOf('vimeo.com')  > -1) {
+          opts.getUrlFn = getVimeoUrl;
+          sendMetricsData({
+            object: 'overlay_icon',
+            method: 'launch',
+            domain: opts.domain
+          });
+          launchVideo(opts);
+        }
+      });
+      worker.port.on('metrics', sendMetricsData);
+    }
+  });
+
+  contextMenuHandlers.init(panelUtils.getPanel());
+};
+exports.onUnload = function(reason) {
+  panelUtils.destroy();
+  contextMenuHandlers.destroy();
+  browserResizeMod.destroy();
+  launchIconsMod.destroy();
+};
