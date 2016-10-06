d35aec682b20c985c98c529cdd9c10ecfb63a925
Joao Sa <me@joaomsa.com>
2016-10-06 15:44:08 -0300

    Fixes duration output time formatting (#395)
    - fixes #382
    - fixes #385

--- components/player-view.js
+++ components/player-view.js
@@ -8,9 +8,12 @@ const sendToAddon = require('../client-lib/send-to-addon');
 const GeneralControls = require('./general-controls');
 
 function formatTime(seconds) {
-  const now = new Date(seconds * 1000);
-  const utc = new Date(now.getTime() + (now.getTimezoneOffset() * 60 * 1000));
-  return utc.toLocaleTimeString('en-US', {hour12: false})
+  const secondsInDay = 24 * 60 * 60;
+  const days = Math.floor(seconds / secondsInDay);
+  seconds = seconds % secondsInDay;
+  const utc = new Date(seconds * 1000);
+  return `${days}:${utc.toLocaleTimeString('en-US', {hour12: false, timeZone: 'UTC'})}`
+    .replace(/^0:/, '') // Strip leading "0:" if days is empty.
     .replace(/^00:/, '') // Strip leading "00:" if hours is empty.
     .replace(/^0/, '');  // Strip leading "0" in minutes, if any.
 }
