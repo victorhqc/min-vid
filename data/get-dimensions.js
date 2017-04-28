76371dd536d92910c24dd60a08ac3b4018a36007
Dave Justice <djustice@mozilla.com>
2017-04-28 17:45:01 -0400

    Set max and min window sizes to prevent breakage (#779)
    - fixes #667

---
+++ data/get-dimensions.js
@@ -0,0 +1,22 @@
+self.port.emit('dimensions', getDimensions());
+
+function getDimensions() {
+  let width = 0;
+  let height = 0;
+
+  if (typeof(window.innerWidth) === 'number') {
+    height = window.innerHeight;
+    width = window.innerWidth;
+  } else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
+    height = document.documentElement.clientHeight;
+    width = document.documentElement.clientWidth;
+  } else if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
+    height = document.body.clientHeight;
+    width = document.body.clientWidth;
+  }
+
+  return {
+    width: width,
+    height: height
+  };
+}
--- index.js
+++ index.js
@@ -26,7 +26,16 @@ const sendMetricsData = require('./lib/send-metrics-data');
 const contextMenuHandlers = require('./lib/context-menu-handlers');
 const windowUtils = require('./lib/window-utils');
 
-let launchIconsMod;
+const prefs = require('sdk/simple-prefs').prefs;
+
+let launchIconsMod, pageDimensionMod;
+
+// default dimensions (for the case where width/height pref is
+// set before a new page is opened)
+let dimensions = {
+  height: 768,
+  width: 1024
+};
 
 exports.main = function() {
   // add launch icon to video embeds
@@ -62,6 +71,14 @@ exports.main = function() {
     }
   });
 
+  pageDimensionMod = pageMod.PageMod({
+    include: '*',
+    contentScriptFile: './get-dimensions.js?cachebust=' + Date.now(),
+    onAttach: function(worker) {
+      worker.port.on('dimensions', opts => dimensions = opts);
+    }
+  });
+
   contextMenuHandlers.init(windowUtils.getWindow());
 };
 
@@ -69,4 +86,17 @@ exports.onUnload = function(reason) {
   windowUtils.destroy(true);
   contextMenuHandlers.destroy();
   launchIconsMod.destroy();
+  pageDimensionMod.destroy();
 };
+
+
+// handle window default sizing
+require('sdk/simple-prefs').on('height', function() {
+  if (prefs.height < 260) prefs.height = 260;
+  else if (prefs.height > dimensions.height) prefs.height = dimensions.height;
+});
+
+require('sdk/simple-prefs').on('width', function() {
+  if (prefs.width < 400) prefs.width = 400;
+  else if (prefs.width > dimensions.width) prefs.width = dimensions.width;
+});
