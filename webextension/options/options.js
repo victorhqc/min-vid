4b5823e4b6f3ae21a7feef1e8a98c8d94a89cf39
Dave Justice <djustice@mozilla.com>
2017-08-18 12:05:10 -0400

    Enforce resolution max and retain dimensions settings through refresh (#992)
    - fixes #972
    - fixes #969

--- webextension/options/options.js
+++ webextension/options/options.js
@@ -1,7 +1,19 @@
 document.querySelector('form').addEventListener('submit', function(ev) {
   ev.preventDefault();
+  const widthEl = document.querySelector('#min-vid-width');
+  const heightEl = document.querySelector('#min-vid-height');
+  if ((widthEl.valueAsNumber > parseInt(widthEl.max, 10)) || (heightEl.valueAsNumber > parseInt(heightEl.max, 10))) return;
   browser.storage.local.set({
-    width: document.querySelector('#min-vid-width').value,
-    height: document.querySelector('#min-vid-height').value
+    width: widthEl.valueAsNumber,
+    height: heightEl.valueAsNumber
   });
 });
+
+function restoreOptions() {
+  browser.storage.local.get().then(r => {
+    document.querySelector('#min-vid-width').value = r.width;
+    document.querySelector('#min-vid-height').value = r.height;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
